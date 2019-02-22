b02e8e8697acfcd44bfe5372b665eae07ba215d1
Nick Wyman <nwyman84@gmail.com>
2019-02-21 23:25:58 -0500

    update app message

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React, for me! test
+            Learn React, for Docker!
           </a>
         </header>
       </div>
